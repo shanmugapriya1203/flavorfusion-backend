8ecb97d6a89196e5638ab524f414267d1a614e67
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-11-19 09:07:57 +0530

    complete Logout functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,3 +83,13 @@ try {
     res.status(500).json({error:'Internal server error'})
 }
 }
+
+export const logoutUser= async(req,res)=>{
+    try {
+        res.cookie("jwt","",{maxAge:1})
+        res.status(200).json({message:"User logged out successfully"})
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log(error, error.message);
+    }
+  }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { login, register, updateUser } from '../controllers/userController.js'
+import { login, logoutUser, register, updateUser } from '../controllers/userController.js'
 import protectRoute from './../middleware/protectRoute.js';
 
 const router= express.Router()
 router.post('/register',register)
 router.post('/login',login)
+router.post('/logout',logoutUser)
+
 router.post('/update/:id',protectRoute,updateUser)
 
 
