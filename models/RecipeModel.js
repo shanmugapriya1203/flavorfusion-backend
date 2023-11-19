028095f3c4ce25b739be9a39e3603336936c7368
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-11-19 09:08:11 +0530

    create recipe model

---
+++ models/RecipeModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+export default Recipe;
